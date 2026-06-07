93e983dcd59d987cad493731ab987875a16a5a45
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-06-07 19:37:12 -0300

    feat: praticando com modularização no js

---
+++ modularizacao-js/fucao.js
@@ -0,0 +1,9 @@
+function subtrair(num){
+    return num - 2;
+}
+
+function somar(num){
+    return num + 2;
+}
+
+export {subtrair , somar};
---
+++ modularizacao-js/main.js
@@ -0,0 +1,5 @@
+
+import { subtrair , somar} from "./fucao.js";
+
+console.log("Subtraçãoo : "+subtrair(4));
+console.log("Somar : "+somar(2));
