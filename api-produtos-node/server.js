33e5ef23fd056072b436f5f39f135eb844ae1210
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-06-18 23:30:04 -0300

    feat: documentando a api com swagger

--- api-produtos-node/server.js
+++ api-produtos-node/server.js
@@ -4,6 +4,6 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`=============================================`);
     console.log(`  Servidor rodando com sucesso na porta ${PORT} `);
-    console.log(`  Acesse: http://localhost:${PORT}/api/produtos `);
+    console.log(`  Acesse: http://localhost:${PORT}/api-docs `); 
     console.log(`=============================================`);
 });
--- api-produtos-node/src/index.js
+++ api-produtos-node/src/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const routesProdutos = require('./routes/routesProdutos');
 
 
+const swaggerDocs = require('../swagger.json'); 
+
 const app = express();
 
+
 app.use(express.json());
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 
 app.use('/api', routesProdutos);
 
